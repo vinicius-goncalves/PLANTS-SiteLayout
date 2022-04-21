6f175e3d6badea1d4422cab6fb3190dcae119496
Vinicius Goncalves <oldz.registros@gmail.com>
2022-04-21 16:40:44 -0300

    🔨 Fixing animations, errors and add some others features

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
+let getScroll = document.getElementById('scroll-indicator')
 
+window.addEventListener('scroll', () => {
+    let scrollY = document.documentElement.scrollTop
+    let heightClient = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    let percentage = (scrollY / heightClient) * 100
+    getScroll.style.width = percentage + "%"
+})
